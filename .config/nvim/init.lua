bfc68d7f091e79b4e84c3393bfb04b850257d86f
Greg Leonard <gr3gl@hotmail.com>
2023-04-24 23:03:19 +0100

    Add format on save for lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -66,6 +66,9 @@ rt.setup({
 			vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
 			-- Code action groups
 			vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+
+			-- Format on save
+			vim.cmd([[autocmd BufWritePre *.rs lua vim.lsp.buf.format({ async = false })]])
 		end,
 		["rust-analyzer"] = {
 			checkOnSave = {
